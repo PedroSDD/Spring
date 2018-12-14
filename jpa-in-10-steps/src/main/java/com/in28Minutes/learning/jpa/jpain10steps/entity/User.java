726156bb6652f582ed78790394ea8476f94d75f5
PedroSDD <PedroSDD@users.noreply.github.com>
2018-12-14 09:45:04 +0000

    Add files via upload

---
+++ jpa-in-10-steps/src/main/java/com/in28Minutes/learning/jpa/jpain10steps/entity/User.java
@@ -0,0 +1,21 @@
+package com.in28Minutes.learning.jpa.jpain10steps.entity;
+
+public class User {
+	
+	private long id;
+	private String name;
+	private String role;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public String getName() {
+		return name;
+	}
+	public String getRole() {
+		return role;
+	}
+	
+	
+}
